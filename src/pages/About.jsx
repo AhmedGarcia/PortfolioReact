8f0080ed20043a1d57ebc24b4bddf0fc1f13f11b
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-09-23 17:22:41 -0400

    created robots.txt file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ function About() {
   return (
     <section>
       <h2>About Me</h2>
-      <img src="/assets/not pic.png" alt="Your avatar" /> {/* Avatar */}
+      <img src="/assets/not pic.png" alt="Ahmed Garcia Profile Picture" width="200" height="200" /> {/* Avatar */}
       <p>Hello! Mi name is Ahmed Garcia. I am a passionate Full Stack Developer skilled in JavaScript, PostgreSQL, Express, React, MongoDB, and Mongoose. I build dynamic, user-focused web applications with robust front-end and back-end systems. Proficient in creating responsive interfaces using React and developing APIs and GraphQL services with Node.js and Express. Experienced in both relational (PostgreSQL) and NoSQL (MongoDB) databases, ensuring secure and efficient data management.
 
 I excel in implementing JWT authentication and deploying apps on platforms like Render and Netlify. Recent projects include a social book review platform and a PWA with offline capabilities. I am passionate about learning new technologies and contributing to innovative solutions.</p> {/* Bio */}
